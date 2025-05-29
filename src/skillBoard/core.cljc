57a35123495ffdeaaa88fe5a17ea68150b4972cb
Robert C. Martin <unclebob@cleancoder.com>
2025-05-29 12:47:11 -0500

    Basic orthogonal polygons of the 16 segment display are in place.

--- src/skillBoard/core.cljc
+++ src/skillBoard/core.cljc
@@ -1,42 +1,50 @@
 (ns skillBoard.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m]
+            [skillBoard.display16 :as display]
+            ))
+
+(defn update-state [state]
+  state)
+
 
 (defn setup []
   (q/frame-rate 30)
-  (q/color-mode :hsb)
-  {:color 0
-   :angle 0
-   :distance 150
-   :size 100})
+  (q/background 255))
 
-(defn update-state [{:keys [color angle distance size]}]
-  {:color (mod (+ color  0.7) 255)
-   :angle (+ angle 0.1)
-   :distance (if (< distance -150) 150 (- distance 1))
-   :size (if (< size -100) 100 (- size 1))})
+(defn draw-state [state]
+  (q/background 255)
+  (q/no-fill)
+  (q/stroke 0)
+  (let [{:keys [margin width height] :as display} (display/build-context 400)
+        [w h] (:box display)]
+    (q/rect 0 0 w h)
+    (q/stroke 128 128 128)
+    (q/rect margin margin (- w margin margin) (- h margin margin))
+    (q/line (* 0.5 width) 0 (* 0.5 width) height)
+    (q/line 0 (* 0.5 height) width (* 0.5 height))
+    (q/fill 200 200 255 150)
+    (q/stroke 0 0 255)
+    (display/draw display)))
 
-(defn draw-state [{:keys [color angle distance size]}]
-  (q/background 240)
-  (q/fill color 255 255)
-  (let [size (Math/abs size)
-        distance (Math/abs distance)
-        x (* distance (q/cos angle))
-        y (* distance (q/sin angle))]
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      (q/ellipse x y size size))))
-
+(def size
+  #?(
+     :cljs
+     [(max (- (.-scrollWidth (.-body js/document)) 20) 900)
+      (max (- (.-innerHeight js/window) 25) 700)]
+     :clj
+     [(- (q/screen-width) 10) (- (q/screen-height) 40)]))
 
 (q/defsketch skillBoard
-  :title "You spin my circle right round"
-  :size [500 500]
-  :setup setup
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  :middleware [m/fun-mode]
-  :host "skillBoard")
+             :title "Skill Board"
+             :size size
+             :setup setup
+             :update update-state
+             :draw draw-state
+             :features [:keep-on-top]
+             :middleware [m/fun-mode]
+             :host "skillBoard")
+
 
 (defn -main [& args]
   (println "skillBoard has begun."))
---
+++ src/skillBoard/display16.clj
@@ -0,0 +1,134 @@
+(ns skillBoard.display16
+  (:require [quil.core :as q]))
+(def aspect-ratio 1.3)
+(def margin-ratio 0.05)
+(def segment-width-ratio 0.07)
+(def segment-gap-ratio (* 0.5 segment-width-ratio))
+
+(defn build-context [width]
+  (let [width (double width)
+        height (* aspect-ratio width)
+        box [width height]
+        margin (* margin-ratio width)
+        segment-gap (* segment-gap-ratio width)
+        segment-width (* segment-width-ratio width)
+        segment-length (- (* 0.5 width) margin (* 0.5 segment-gap) (* 0.5 segment-width))
+        segment-height (- (* 0.5 height) margin (* 1.5 segment-width))
+        tip-height (* 0.5 segment-width)
+        hbar-length (- segment-length tip-height tip-height)
+        vbar-length (- segment-height (* 2 tip-height))]
+    {:box box
+     :width width
+     :height height
+     :margin margin
+     :segment-gap segment-gap
+     :segment-width segment-width
+     :segment-length segment-length
+     :segment-height segment-height
+     :tip-height tip-height
+     :hbar-length hbar-length
+     :vbar-length vbar-length})
+  )
+
+(defn translate-point [[x y] [dx dy]]
+  [(+ x dx) (+ y dy)])
+
+(defn build-horizontal-segment [{:keys [margin segment-width tip-height hbar-length]}]
+  (let [half-segment-width (* 0.5 segment-width)
+        left-tip [(+ margin half-segment-width) (+ margin half-segment-width)]
+        left-tip-top [(+ (first left-tip) tip-height) margin]
+        right-tip-top [(+ (first left-tip-top) hbar-length) margin]
+        right-tip [(+ (first right-tip-top) tip-height) (+ margin half-segment-width)]
+        right-tip-bottom [(first right-tip-top) (+ margin segment-width)]
+        left-tip-bottom [(first left-tip-top) (+ margin segment-width)]]
+    [left-tip left-tip-top right-tip-top right-tip right-tip-bottom left-tip-bottom]))
+
+(defn build-vertical-segment [{:keys [margin segment-width tip-height vbar-length]}]
+  (let [half-segment-width (* 0.5 segment-width)
+        top-tip [(+ margin half-segment-width) (+ margin segment-width)]
+        top-tip-right (translate-point top-tip [half-segment-width tip-height])
+        bottom-tip-right (translate-point top-tip-right [0 vbar-length])
+        bottom-tip (translate-point bottom-tip-right [(- half-segment-width) tip-height])
+        bottom-tip-left (translate-point bottom-tip [(- half-segment-width) (- tip-height)])
+        top-tip-left (translate-point bottom-tip-left [0 (- vbar-length)])]
+    [top-tip top-tip-right bottom-tip-right bottom-tip bottom-tip-left top-tip-left]
+    )
+  )
+
+
+(defn draw-segment [seg]
+  (q/begin-shape)
+  (doseq [point-index [0 1 2 3 4 5 0]]
+    (let [[x y] (nth seg point-index)]
+      (q/vertex x y)))
+  (q/end-shape :close))
+
+(defn draw [{:keys [segment-gap segment-length segment-height segment-width height width margin] :as display}]
+  (let [hseg (build-horizontal-segment display)
+        vseg (build-vertical-segment display)
+        right-displacement (+ segment-length segment-gap)
+        half-segment-width (* 0.5 segment-width)
+        half-height (* 0.5 height)
+        vertical-displacement (+ segment-height segment-width)]
+
+    ;segement 0
+    (draw-segment hseg)
+
+    ;segment 1
+    (q/with-translation
+      [right-displacement 0]
+      (draw-segment hseg))
+
+    ;segment 2
+    (draw-segment vseg)
+
+    ;segment 4
+    (q/with-translation
+      [(+ segment-length (* 0.5 segment-gap)) 0]
+      (draw-segment vseg))
+
+    ;segment 6
+    (q/with-translation
+      [(- width margin margin segment-width) 0]
+      (draw-segment vseg))
+
+    ;segment 7
+    (q/with-translation
+      [0 (- half-height margin half-segment-width)]
+      (draw-segment hseg))
+
+    ;segment 8
+    (q/with-translation
+      [right-displacement (- half-height margin half-segment-width)]
+      (draw-segment hseg))
+
+    ;segment 9
+    (q/with-translation
+      [0 vertical-displacement]
+      (draw-segment vseg))
+
+    ;segment 11
+    (q/with-translation
+       [(+ segment-length (* 0.5 segment-gap)) vertical-displacement]
+       (draw-segment vseg))
+
+    ;segment 13
+    (q/with-translation
+      [(- width margin margin segment-width) vertical-displacement]
+      (draw-segment vseg))
+    ;segment 14
+    (q/with-translation
+      [0 (- height segment-width margin margin)]
+      (draw-segment hseg))
+
+    ;segment 15
+    (q/with-translation
+      [right-displacement (- height segment-width margin margin)]
+      (draw-segment hseg))
+
+    )
+  )
+
+
+
+
