e538141adb3a2420a115d1a6b86bc6c8de23d4e4
Robert C. Martin <unclebob@cleancoder.com>
2025-11-25 16:22:49 -0700

    Update tests for color and remarks.

--- spec/skillBoard/presenter_spec.clj
+++ spec/skillBoard/presenter_spec.clj
@@ -1,5 +1,6 @@
 (ns skillBoard.presenter-spec
   (:require [skillBoard.presenter :as p]
+            [skillBoard.time-util :as time-util]
             [speclj.core :refer :all]
             ))
 
@@ -77,15 +78,117 @@
   )
 
 (describe "presenter functions"
+  (context "format-res"
+    (it "formats a basic reservation"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN0915309/GND/100             ",
+                :color :white}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 1000
+                              :ground-speed 100
+                              :lat-lon [0 0]
+                              :rogue? false
+                              :on-ground? false?
+                              :adsb? false})
+               )
+      )
+    (it "formats a on ground reservation"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN000000/GND/000 RAMP         ",
+                :color :green}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 0
+                              :ground-speed 0
+                              :lat-lon [42.4221486 -87.8679161]
+                              :rogue? false
+                              :on-ground? true
+                              :adsb? true})
+               )
+      )
+
+    (it "formats a taxi reservation"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN000000/GND/010 TAXI         ",
+                :color :green}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 0
+                              :ground-speed 10
+                              :lat-lon [42.4221486 -87.8679161]
+                              :rogue? false
+                              :on-ground? true
+                              :adsb? true})
+               )
+      )
+
+    (it "formats a rogue (non-checked-out) reservation in the pattern"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN000000/020/100 PATN NO-CO   ",
+                :color :blue}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 2000
+                              :ground-speed 100
+                              :lat-lon [42.4221486 -87.8679161]
+                              :rogue? true
+                              :on-ground? false
+                              :adsb? true})
+               )
+      )
+
+    (it "formats a reservation flying low"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN000000/008/100 LOW          ",
+                :color :white}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 800
+                              :ground-speed 100
+                              :lat-lon [42.4221486 -87.8679161]
+                              :rogue? false
+                              :on-ground? false
+                              :adsb? true})
+               )
+      )
+    (it "formats a reservation flying near"
+      (should= {:line "05:50Z TAIL   LAS.P LAS.I        UGN000000/030/100 NEAR         ",
+                :color :white}
+               (p/format-res {:start-time time-util/epoch
+                              :tail-number "TAIL"
+                              :pilot-name ["PILOT" "LAST"]
+                              :instructor-name ["INSTR" "LAST"]
+                              :co nil
+                              :altitude 3000
+                              :ground-speed 100
+                              :lat-lon [42.4221486 -87.8679161]
+                              :rogue? false
+                              :on-ground? false
+                              :adsb? true})
+               )
+      )
+    )
+
   (context "split-taf"
     (it "splits normal TAF strings correctly"
-      (should= ["TAF KORD"
-                "121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250"
-                "TEMPO 1212/1216 5SM -RA BR OVC015"
-                "PROB30 121600 25012KT P6SM BKN020"
-                "PROB40 TEMPO 130000 24008KT P6SM SCT030 BKN200"
-                "BECMG131200 22005KT P6SM SCT250"
-                "FM141200 22005KT P6SM SCT250"]
+      (should= [{:line "TAF KORD", :color :white}
+                {:line "121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250", :color :white}
+                {:line "TEMPO 1212/1216 5SM -RA BR OVC015", :color :white}
+                {:line "PROB30 121600 25012KT P6SM BKN020", :color :white}
+                {:line "PROB40 TEMPO 130000 24008KT P6SM SCT030 BKN200", :color :white}
+                {:line "BECMG131200 22005KT P6SM SCT250", :color :white}
+                {:line "FM141200 22005KT P6SM SCT250", :color :white}]
                (p/split-taf
                  (str
                    "TAF KORD 121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250 "
@@ -94,9 +197,9 @@
                    "BECMG131200 22005KT P6SM SCT250 "
                    "FM141200 22005KT P6SM SCT250"))))
     (it "handles amended TAFs correctly"
-      (should= ["TAF AMD KORD"
-                "121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250"
-                "FM141200 22005KT P6SM SCT250"]
+      (should= [{:line "TAF AMD KORD", :color :white}
+                {:line "121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250", :color :white}
+                {:line "FM141200 22005KT P6SM SCT250", :color :white}]
                (p/split-taf
                  (str
                    "TAF AMD KORD 121130Z 1212/1318 27015G25KT P6SM SCT020 BKN250 "
