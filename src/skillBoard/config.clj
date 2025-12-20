f011c107eaa351c97c4232e85e223395abc8a1ed
Robert C. Martin <unclebob@cleancoder.com>
2025-12-20 07:47:08 -0600

    20251220

--- src/skillBoard/config.clj
+++ src/skillBoard/config.clj
@@ -5,7 +5,7 @@
 (defn load-config []
   (reset! config (read-string (slurp "private/config"))))
 
-(def version "20251219")
+(def version "20251220")
 
 ;Display configuration
 (def display-info (atom {}))                                ;determined after sketch started.
